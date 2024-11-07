52a341bbe8fe5d7d4197e9be53dcebec13a29663
RousseauMalco <rousseaumalco@gmail.com>
2024-11-07 16:32:16 -0600

    Pt 2 of Time Travel WIP.

--- src/TimeTravel.elm
+++ src/TimeTravel.elm
@@ -3,3 +3,22 @@ module TimeTravel exposing (addTimeTravel)
 import Playground exposing (..)
 import Set
 
+addTimeTravel rawGame = 
+
+    {initialState = initialStateWithTimeTravel rawGame
+    , updateState = updateWithTimeTravel rawGame 
+    , view = viewWithTimeTravel rawGame}
+
+initialStateWithTimeTravel rawGame = 
+    {rawModel = rawGame.initialState
+    , paused = False}
+
+viewWithTimeTravel rawGame computer model =
+    rawGame.view computer model
+
+updateWithTimeTravel rawGame computer model =
+    rawGame.updateState computer model
+
+
+
+
